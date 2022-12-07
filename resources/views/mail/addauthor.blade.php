d128ad7bc8c4a3a2443503d628145a2e226175f5
filippodicostanzo <filippodicostanzo@gmail.com>
2022-12-07 16:36:37 +0100

    Fix First Name and Last Name of Main Author

--- resources/views/mail/addauthor.blade.php
+++ resources/views/mail/addauthor.blade.php
@@ -59,7 +59,7 @@
                                         New Paper Submitted</h1>
 
                                     <p style="box-sizing: border-box; font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; position: relative; font-size: 16px; line-height: 1.5em; margin-top: 0; text-align: left;">
-                                        Dear <b>{{$post->user_fk->firstname}} {{$post->user_fk->lastname}}</b><br>
+                                        Dear <b>{{$post->user_fk->name}} {{$post->user_fk->surname}}</b><br>
                                         we received your submission for the <b>{{$post->template_fk->name}}</b>
                                         titled <b>{{$post->title}}</b> co authored with @foreach($author as $aut)
                                             {{ $loop->first ? '' : ', ' }}<b>{{$aut->firstname}} {{$aut->lastname}}</b>
