330e9f2554c9e6fc3563d8fbbcbbbf2e8e0ccf69
filippodicostanzo <filippodicostanzo@gmail.com>
2022-12-07 15:10:10 +0100

    Added Notice in Pages Account Confirmation and Reset Password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,1 +1,14 @@
 @extends('adminlte::auth.passwords.email')
+
+@section('auth_body')
+    @parent
+
+    @if(session('status'))
+        <br>
+        <p>It can sometimes take time for the email to come through. Please be patient and check your spam or trash folders.
+            If after 24 hours you have not received your email you may have entered the wrong email address. In case of
+            problems, please contact us at naplesforumonservice@gmail.com</p>
+    @endif
+
+@endsection
+
--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,1 +1,10 @@
 @extends('adminlte::auth.verify')
+@section('auth_body')
+    @parent
+
+    <br>
+    <br>
+    <p>It can sometimes take time for the email to come through. Please be patient and check your spam or trash folders.
+        If after 24 hours you have not received your email you may have entered the wrong email address. In case of
+        problems, please contact us at naplesforumonservice@gmail.com</p>
+@endsection
